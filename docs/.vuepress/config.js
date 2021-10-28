702ba880ee4c2cb04d45ed393d9df325e6a92afa
fuadop <fuadolatunji@gmail.com>
2021-10-28 17:02:25 +0100

    feat: adds basic config

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,40 @@
+module.exports = {
+  title: 'Textury Docs',
+  description: 'The act of permanence, building for the permaweb.',
+  themeConfig: {
+    sidebarDepth: 2,
+    logo: '/img/textury.svg',
+    repo: 'textury/docs',
+    editLinks: true, 
+    editLinkText: "Edit this page on GitHub",
+    docsBranch: 'main',
+    docsDir: "docs",
+    nav: [
+      {
+        text: 'Overview',
+        link: '/'
+      },
+      {
+        text: 'Blockweave',
+        link: '/blockweave'
+      },
+      {
+        text: 'Arkb',
+        link: '/arkb'
+      },
+      {
+        text: 'Ardb',
+        link: '/ardb'
+      },
+      {
+        text: 'Arlocal',
+        link: '/arlocal'
+      },
+      {
+        text: 'GitHub',
+        link: 'https://github.com/textury',
+        icon: '/img/github.svg'
+      }
+    ]
+  },
+};
